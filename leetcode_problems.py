d2a009731070b0e905f567540c8985532e84872f
Vineet Baj <vineetbaj@gmail.com>
2019-07-03 17:24:10 +0530

    Update leetcode_problems.py

--- leetcode_problems.py
+++ leetcode_problems.py
@@ -199,3 +199,10 @@ class Solution:
                 break
                 
         return pnc
+        
+#Peak Index in a Mountain Array
+class Solution:
+    def peakIndexInMountainArray(self, A: List[int]) -> int:
+        for i in range(1,len(A)):
+            if A[i]>A[i-1] and A[i]>A[i+1]:
+                return i      
