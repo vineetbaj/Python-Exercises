7b5b14bca85c5d6d128c35da35688a08fbec9ab3
Vineet Baj <vineetbaj@gmail.com>
2019-07-08 10:08:52 +0530

    Update leetcode_problems.py

--- LeetCode/Easy/leetcode_problems.py
+++ LeetCode/Easy/leetcode_problems.py
@@ -240,8 +240,8 @@ class Solution:
 #Array Partition I
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        num = sorted(nums)
+        nums = sorted(nums)
         sum = 0
-        for i in range(0,len(num),2):
-                sum += num[i]
+        for i in range(0,len(nums),2):
+                sum += nums[i]
         return sum
