2024f4e478540a86dcc14514567afa14fc5bf684
Vineet Baj <vineetbaj@gmail.com>
2019-07-09 16:32:52 +0530

    Update leetcode_problems.py

--- LeetCode/Easy/leetcode_problems.py
+++ LeetCode/Easy/leetcode_problems.py
@@ -272,3 +272,13 @@ class Solution:
             if words[i-2] == first and words[i-1] == second:
                 res.append(words[i])
         return res
+        
+#Fibonacci Number
+class Solution:
+    def fib(self, N: int) -> int:
+        fib = {0:0,1:1}
+        for i in range(2,N+1):
+                fib[i] = fib[i-1] + fib[i-2]
+        
+        return fib[N]
+        
