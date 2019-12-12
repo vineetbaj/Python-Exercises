0713e5f3f02962eb5dc31234a51c77e7e0168231
Vineet Baj <vineetbaj@gmail.com>
2019-12-12 15:23:06 +0530

    Create sequence equation.py

---
+++ LeetCode/Easy/sequence equation.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the permutationEquation function below.
+def permutationEquation(p):
+    n = []
+    for i in range(1,len(p)+1):
+        n.append(p.index(p.index(i)+1)+1)
+    return n
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    p = list(map(int, input().rstrip().split()))
+
+    result = permutationEquation(p)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
